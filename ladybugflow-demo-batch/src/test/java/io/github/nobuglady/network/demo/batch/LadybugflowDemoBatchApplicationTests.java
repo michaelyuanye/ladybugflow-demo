783de3fe7ca75962115ca854f7dd4ee8a662bdbd
nobuglady <nobuglady@protonmail.com>
2022-08-09 21:17:11 +0900

    update

---
+++ ladybugflow-demo-batch/src/test/java/io/github/nobuglady/network/demo/batch/LadybugflowDemoBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nobuglady.network.demo.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LadybugflowDemoBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
